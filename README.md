df8be02b2a44031c244ec2d292a772435a06a335
Dylan Madisetti <contact@dylanmadisetti.com>
2014-12-17 02:03:44 -0400

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+family - directory of Madisetti's
+=========
+
+[![Build Status](https://travis-ci.org/dmadisetti/family.png)](https://travis-ci.org/dmadisetti/family)
+
+Github pages showing everyone with a `*.madisetti.me` domain. This project also acts a means of managing these subdomains, where travis manages records using Namecheap's api. 
+
+---
+Objectives:
+
+- Play around with Jekyll and Github pages - `Check`
+- Play around with Namecheap API - `Check`
+- Remove costs of paying for indiamadisetti and other cousins (yes I'm that cheap) - `Check`
+
+Todo:
+
+- Document???
---
+++ deploy.rb
@@ -0,0 +1,77 @@
+require 'yaml'
+require 'net/http'
+require 'nokogiri'
+
+# Construct base url
+class NameCheap
+	def initialize()  
+		# Our commands
+		@check  = "namecheap.domains.dns.getHosts"
+		@update = "namecheap.domains.dns.setHosts"
+
+    	# Instance variables  
+		@baseurl  = "http://api.namecheap.com/xml.response"
+		@baseurl += "?apiuser="  + ENV['username'] 
+		@baseurl += "&apikey="   + ENV['key']
+		@baseurl += "&username=" + ENV['username']
+		@baseurl += "&ClientIp=107.22.240.13" # Just some random IP
+	end
+
+	def query
+		return call "#{@baseurl}&Command=#{@check}&SLD=smith&TLD=me"
+	end
+
+	# Possibly change this to post
+	def process changes
+		return call "#{@baseurl}&Command=#{@update}&SLD=smith&TLD=me#{changes}"
+	end
+
+	def call url
+		puts "Getting #{url}"
+		return parse Net::HTTP.get(URI.parse(url))
+	end
+
+	def parse response
+		result = Hash.new
+		doc = Nokogiri::XML(response)
+		doc.remove_namespaces!
+
+		# Let's see what's going on
+		puts doc
+		puts '--------'
+
+		doc.xpath("//host").each do |host|
+			if result[host.attribute("Name").value] == nil
+				result[host.attribute("Name").value] = []
+			end
+			result[host.attribute("Name").value].push(host.attributes().map{|x| x.value})
+		end
+		return result
+	end
+end
+
+# Let's encapsulate everythang
+def run()
+
+	# Start off our url
+	nc = NameCheap.new
+
+	data = YAML.load_file('_data/records.yml')
+
+	# Find what records exist
+	# records = nc.query()
+
+	# init blank string of needed changes
+	changes = ""
+	count   = 1
+
+	# Lookup to see if match
+	data.each do |member|
+		changes += "&HostName#{count}=#{member['subdomain']}&RecordType#{count}=#{member['type']}&Address#{count}=#{member['address']}&TTL#{count}=1800"
+		count += 1
+	end
+
+	nc.process(changes)
+end
+
+run()
